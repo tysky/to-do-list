18ed84b672a49abcf41b12c1118cfd2d4bb4d895
tysky <dtysky51@gmail.com>
2018-12-16 10:14:08 +0300

    add store test for changing task status

--- __tests__/store.test.js
+++ __tests__/store.test.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { uniqueId } from 'lodash';
 import reducers from '../src/reducers';
 import * as actions from '../src/actions';
 
@@ -9,6 +10,16 @@ const initialState = {
   taskSendingState: 'none',
   newTaskInputText: '',
 };
+
+const getNextId = () => Number(uniqueId());
+
+const makeTask = (text, status = 'todo') => ({
+  id: getNextId(),
+  text,
+  status,
+});
+const task1 = makeTask('test task1');
+const task2 = makeTask('test task2');
 
 describe('Store', () => {
   let store;
@@ -27,29 +38,58 @@ describe('Store', () => {
   });
 
   test('should add new task', () => {
-    const task1 = {
-      id: 1, text: 'test task1',
-    };
     store.dispatch(actions.newTaskFetched(task1));
     expect(store.getState().tasks).toEqual({
       1: {
         id: 1,
         text: 'test task1',
+        status: 'todo',
       },
     });
 
-    const task2 = {
-      id: 2, text: 'test task2',
-    };
     store.dispatch(actions.newTaskFetched(task2));
     expect(store.getState().tasks).toEqual({
       1: {
         id: 1,
         text: 'test task1',
+        status: 'todo',
       },
       2: {
         id: 2,
         text: 'test task2',
+        status: 'todo',
+      },
+    });
+  });
+
+  test('should change status of the existing task', () => {
+    store.dispatch(actions.newTaskFetched(task1));
+    store.dispatch(actions.newTaskFetched(task2));
+    store.dispatch(actions.taskEdited({ id: 1, status: 'done' }));
+    expect(store.getState().tasks).toEqual({
+      1: {
+        id: 1,
+        text: 'test task1',
+        status: 'done',
+      },
+      2: {
+        id: 2,
+        text: 'test task2',
+        status: 'todo',
+      },
+    });
+    store.dispatch(actions.taskEdited({ taskId: 2, status: 'done' }));
+    store.dispatch(actions.taskEdited({ taskId: 1, status: 'todo' }));
+    expect(store.getState().tasks).toEqual({
+      1: {
+        id: 1,
+        text: 'test task1',
+        status: 'todo',
+      },
+      2: {
+        id: 2,
+        text: 'test task2',
+        status: 'done',
       },
     });
   });
